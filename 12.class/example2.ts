6eb7160571c58333078d3e989b5d8c33b8deec5d
asifmohamed491-code <asifmohamed491@gmail.com>
2026-06-26 21:23:12 +0530

    classes

---
+++ 12.class/example2.ts
@@ -0,0 +1,30 @@
+// with class
+class User {
+    firstName: string;
+    lastName: string;
+    age: number
+
+    constructor(firstName: string, lastName: string, age: number) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    greet(){
+        return `Hello, ${this.firstName} ${this.lastName}`
+    }
+    getAge(){
+        return this.age;
+    }
+}
+
+// creating instance  or objects
+let user1 = new User('john','Doe',23)
+let user2 = new User('jane','smith',35)
+
+
+console.log(user1.greet())
+console.log(user1.getAge())
+
+console.log(user2.greet())
+console.log(user2.getAge())
